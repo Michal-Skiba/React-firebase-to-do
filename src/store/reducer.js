d6727715ce780445c3b2eba4e0c4dc96bf4d45a9
Michał Skiba <skiba_michal@wp.pl>
2018-01-18 22:50:45 +0100

    to database

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,17 +1,28 @@
 import * as actionTypes from './actions';
+import firebase from 'firebase';
+
 
 const initialState = {
     tasks: []
+};
+
+
+const writeTask =(id, name, priority) => {
+    firebase.database().ref('Task/' + id).set({
+        name: name,
+        priority: priority
+    });
 };
 
 const reducer = ( state = initialState, action ) => {
     switch ( action.type ) {
         case actionTypes.ADD_TASK:
             const newTask = {
-                id: Math.random(),
+                id: Math.round(Math.random()*1000000000),
                 name: action.taskData.name,
                 priority: action.taskData.priority
             };
+            writeTask(newTask.id, newTask.name, newTask.priority);
             return {
                 ...state,
                 tasks: state.tasks.concat( newTask )
